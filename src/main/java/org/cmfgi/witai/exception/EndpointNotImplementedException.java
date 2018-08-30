ed37c7dcbad95f74189bf9150ef29d5bc8388f68
Gilgen Florian <florian.gilgen@coremedia.com>
2018-08-30 06:30:33 +0000

    Maven artifacts for hackathon

--- src/main/java/org/cmfgi/witai/exception/EndpointNotImplementedException.java
+++
@@ -1,9 +0,0 @@
-package org.cmfgi.witai.exception;
-
-public class EndpointNotImplementedException extends RuntimeException{
-  public static final String ERROR_MESSAGE="Endpoint %s is not implemented";
-
-  public EndpointNotImplementedException(String endpointName){
-    super(String.format(ERROR_MESSAGE,endpointName));
-  }
-}
